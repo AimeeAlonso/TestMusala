7298ad4bdaa13bb374512fdd68c9bc01cfcbc42d
Aimee Alonso <aimeear1993@gmail.com>
2021-03-20 16:29:16 -0400

    Add pagination to gateways list

--- TestMusalaWebHost/ClientApp/src/app/components/gateways/gateways.component.ts
+++ TestMusalaWebHost/ClientApp/src/app/components/gateways/gateways.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
-import { Gateway, PaginationResultDto } from '../../models/gateway';
+import { Gateway, PaginationInfoDto, PaginationResultDto } from '../../models/gateway';
 
 @Component({
   selector: 'app-gateways',
@@ -8,19 +8,44 @@ import { Gateway, PaginationResultDto } from '../../models/gateway';
   styleUrls: ['./gateways.component.css']
 })
 export class GatewaysComponent implements OnInit {
+  pagination: PaginationInfoDto;
+  prevPageDisabled: boolean;
+  nextPageDisabled: boolean;
+  totalPages: number;
 
   constructor(
     private http: HttpClient,
     @Inject('BASE_URL') private baseURL : string
-  ) { }
+  ) {
+    this.pagination = new PaginationInfoDto(1,3,0);
+  }
 
   gateways: Gateway[]
-
+  prevPage() {
+    this.getGateways(this.pagination.pageNumber - 1);
+  }
+  nextPage() {
+    this.getGateways(this.pagination.pageNumber + 1);
+  }
+  getGateways(pageNumber: number) {
+    var url = this.baseURL + 'api/gateway';
+    var queryParams = new HttpParams()
+      .set('pageNumber', pageNumber.toString())
+      .set('pageSize', this.pagination.pageSize.toString());
+    this.http.get<PaginationResultDto>(url, { params: queryParams })
+      .subscribe(result => {
+        this.gateways = result.data;
+        this.pagination.pageNumber = result.pagination.pageNumber;
+        this.pagination.pageSize = result.pagination.pageSize;
+        this.pagination.total = result.pagination.total;
+        this.totalPages = this.pagination.pageSize == 0 ? 0 : Math.round((this.pagination.total / this.pagination.pageSize)) + 1;
+        this.nextPageDisabled = this.pagination.pageNumber == this.totalPages;
+        this.prevPageDisabled = this.pagination.pageNumber <= 1;
+      },
+        error => { console.log(error) });
+  }
   ngOnInit() {
-    this.http.get<PaginationResultDto>(this.baseURL + 'api/gateway').subscribe(result => {
-      this.gateways = result.data;
-    },
-      error => { console.log(error) });
+    this.getGateways(1);
   }
 
 }
--- TestMusalaWebHost/ClientApp/src/app/models/gateway.ts
+++ TestMusalaWebHost/ClientApp/src/app/models/gateway.ts
@@ -9,10 +9,18 @@ export class PaginationResultDto
 {
   pagination: PaginationInfoDto
   data: Gateway[];
+ 
 }
 
 export class PaginationInfoDto {
   pageNumber: number;
   pageSize: number;
   total: number;
+  constructor(pageNumber: number,
+    pageSize: number,
+    total: number) {
+    this.pageSize = pageSize;
+    this.pageNumber = pageNumber;
+    this.total = total;
+  }
 }
